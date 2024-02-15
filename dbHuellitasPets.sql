1228510fe8e0253458e916e9b40ea55c3047ea96
Frank5031 <129124378+Frank5031@users.noreply.github.com>
2024-02-14 20:35:13 -0600

    ActualizacionDeBase(Restricciones)

--- dbHuellitasPets.sql
+++ dbHuellitasPets.sql
@@ -4,13 +4,13 @@ USE dbHuellitasPets;
 
 CREATE TABLE IF NOT EXISTS clientes (
   id_cliente INT AUTO_INCREMENT PRIMARY KEY,
-  nombre_cliente VARCHAR(50),
-  apellido_cliente VARCHAR(50),
-  dui_cliente VARCHAR(10),
-  correo_cliente VARCHAR(100),
-  telefono_cliente VARCHAR(9),
+  nombre_cliente VARCHAR(50) NOT NULL,
+  apellido_cliente VARCHAR(50) NOT NULL,
+  dui_cliente VARCHAR(10) UNIQUE,
+  correo_cliente VARCHAR(100) UNIQUE,
+  telefono_cliente VARCHAR(9) UNIQUE,
   nacimiento_cliente DATE,
-  direccion_cliente VARCHAR(250),
+  direccion_cliente VARCHAR(250) NOT NULL,
   clave_cliente VARCHAR(200),
   estado_cliente ENUM ('Activo', 'Inactivo'),
   fecha_registro_cliente DATE,
@@ -32,10 +32,10 @@ CREATE TABLE IF NOT EXISTS permisos (
 
 CREATE TABLE IF NOT EXISTS administradores (
   id_admin INT AUTO_INCREMENT PRIMARY KEY,
-  nombre_admin VARCHAR(50),
-  apellido_admin VARCHAR(50),
-  correo_admin VARCHAR(100),
-  alias_admin VARCHAR(50),
+  nombre_admin VARCHAR(50) NOT NULL,
+  apellido_admin VARCHAR(50) NOT NULL,
+  correo_admin VARCHAR(100) UNIQUE,
+  alias_admin VARCHAR(50) UNIQUE,
   clave_admin VARCHAR(100),
   fecha_registro_admin DATE,
   imagen_administrador VARCHAR(50)
@@ -51,26 +51,26 @@ CREATE TABLE IF NOT EXISTS asignacionPermisos (
 
 CREATE TABLE IF NOT EXISTS categorias (
   id_categoria INT AUTO_INCREMENT PRIMARY KEY,
-  nombre_categoria VARCHAR(50),
-  descripcion_categoria VARCHAR(250),
+  nombre_categoria VARCHAR(50) NOT NULL,
+  descripcion_categoria VARCHAR(250) NOT NULL,
   imagen_categoria VARCHAR(50)
 );
 
 CREATE TABLE IF NOT EXISTS marcas (
   id_marca INT AUTO_INCREMENT PRIMARY KEY,
-  nombre_marca VARCHAR(100),
+  nombre_marca VARCHAR(100) NOT NULL,
   imagen VARCHAR(25)
 );
 
 CREATE TABLE IF NOT EXISTS productos (
   id_producto INT AUTO_INCREMENT PRIMARY KEY,
-  nombre_producto VARCHAR(50),
-  descripcion_producto VARCHAR(250),
-  precio_producto DECIMAL(5,2),
+  nombre_producto VARCHAR(50) NOT NULL,
+  descripcion_producto VARCHAR(250) NOT NULL,
+  precio_producto DECIMAL(5,2) NOT NULL,
   imagen_producto VARCHAR(50),
   estado_producto ENUM('activo', 'inactivo'),
   existencia_producto INT,
-  fecha_registro_producto DATE,
+  fecha_registro_producto DATE NOT NULL,
   mascotas ENUM('perro', 'gato'),
   id_categoria INT,
   id_marca INT,
@@ -81,8 +81,8 @@ CREATE TABLE IF NOT EXISTS productos (
 CREATE TABLE IF NOT EXISTS pedidos (
   id_pedido INT AUTO_INCREMENT PRIMARY KEY,
   estado_pedido ENUM('pendiente', 'completado', 'cancelado'),
-  fecha_registro TIMESTAMP,
-  direccion_pedido VARCHAR(250),
+  fecha_registro TIMESTAMP NOT NULL,
+  direccion_pedido VARCHAR(250) NOT NULL,
   id_cliente INT,
   CONSTRAINT fk_Pedidos_Clientes FOREIGN KEY (id_cliente) REFERENCES clientes (id_cliente)
 );
@@ -92,7 +92,7 @@ CREATE TABLE IF NOT EXISTS cuponOferta(
  codigo VARCHAR(50) UNIQUE,
  porcentaje FLOAT,
  estado BOOL,
- fecha_ingreso DATE
+ fecha_ingreso DATE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS cuponesUtilizados(
@@ -105,8 +105,8 @@ CREATE TABLE IF NOT EXISTS cuponesUtilizados(
 
 CREATE TABLE IF NOT EXISTS detallesPedidos (
   id_detallePedido INT AUTO_INCREMENT PRIMARY KEY,
-  cantidad_producto INT,
-  precio_producto DECIMAL(5,2),
+  cantidad_producto INT CHECK(Cantidad_producto >= 0),
+  precio_producto DECIMAL(5,2) NOT NULL,
   id_producto INT,
   id_pedido INT,
   CONSTRAINT fk_DetallesPedidos_Productos FOREIGN KEY (id_producto) REFERENCES productos (id_producto),
@@ -117,11 +117,13 @@ CREATE TABLE IF NOT EXISTS valoraciones (
   id_valoracion INT AUTO_INCREMENT PRIMARY KEY,
   calificacion INT,
   comentario VARCHAR(250),
-  fecha_valoracion DATE,
+  fecha_valoracion DATE NOT NULL,
   estado BOOL,
   id_cliente INT,
-  CONSTRAINT fk_Valoraciones_clientes FOREIGN KEY (id_clienteo) REFERENCES detallesPedidos (id_cliente)
+  CONSTRAINT fk_Valoraciones_clientes FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
 );
+
+SELECT * FROM valoraciones
 
 
 /*SELECT * FROM valoraciones, detallespedidos, pedidos, productos, subcategorias, administradores, Permisos, clientes;*/
