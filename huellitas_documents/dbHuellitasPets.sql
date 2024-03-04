74a420093efe71e0f5015005d8cece7e4f5ba25c
Susan Castillo <susancasoli@gmail.com>
2024-03-03 19:25:42 -0600

    Se agregó default a los campos de fecha

--- huellitas_documents/dbHuellitasPets.sql
+++ huellitas_documents/dbHuellitasPets.sql
@@ -1,3 +1,5 @@
+-- DROP DATABASE db_huellitas_pets;
+
 CREATE DATABASE IF NOT EXISTS db_huellitas_pets;
 
 USE db_huellitas_pets;
@@ -17,7 +19,7 @@ CREATE TABLE IF NOT EXISTS clientes (
   direccion_cliente VARCHAR(250) NOT NULL,
   clave_cliente VARCHAR(200),
   estado_cliente ENUM ('Activo', 'Inactivo'),
-  fecha_registro_cliente DATE,
+  fecha_registro_cliente DATE DEFAULT NOW(),
   imagen_cliente VARCHAR(50) DEFAULT 'imagen_cliente.png'
 );
 
@@ -43,7 +45,7 @@ CREATE TABLE IF NOT EXISTS administradores (
   alias_admin VARCHAR(50),
   CONSTRAINT alias_admin_unico UNIQUE(alias_admin),
   clave_admin VARCHAR(100),
-  fecha_registro_admin DATE,
+  fecha_registro_admin DATE DEFAULT NOW(),
   imagen_admin VARCHAR(50) DEFAULT 'imagen_admin.png'
 );
 
@@ -75,10 +77,10 @@ CREATE TABLE IF NOT EXISTS productos (
   precio_producto DECIMAL(5,2) NOT NULL,
   CONSTRAINT precio_producto_check CHECK(precio_producto > 0),
   imagen_producto VARCHAR(50) DEFAULT 'imagen_producto.png',
-  estado_producto ENUM('activo', 'inactivo'),
+  estado_producto ENUM('Disponible', 'No disponible'),
   existencia_producto INT,
   CONSTRAINT existencia_producto_check CHECK(existencia_producto > 0),
-  fecha_registro_producto DATE NOT NULL,
+  fecha_registro_producto DATE DEFAULT NOW(),
   mascotas ENUM('perro', 'gato'),
   id_categoria INT,
   id_marca INT,
@@ -89,7 +91,7 @@ CREATE TABLE IF NOT EXISTS productos (
 CREATE TABLE IF NOT EXISTS pedidos (
   id_pedido INT AUTO_INCREMENT PRIMARY KEY,
   estado_pedido ENUM('pendiente', 'completado', 'cancelado'),
-  fecha_registro_pedido DATE NOT NULL,
+  fecha_registro_pedido DATE DEFAULT NOW(),
   direccion_pedido VARCHAR(250) NOT NULL,
   id_cliente INT,
   CONSTRAINT fk_pedidos_clientes FOREIGN KEY (id_cliente) REFERENCES clientes (id_cliente)
@@ -99,9 +101,9 @@ CREATE TABLE IF NOT EXISTS cupon_oferta(
  id_cupon INT AUTO_INCREMENT PRIMARY KEY,
  codigo_cupon VARCHAR(50),
  CONSTRAINT codigo_cupon_check UNIQUE(codigo_cupon),
- porcentaje_cupon FLOAT,
+ porcentaje_cupon INT,
  estado_cupon BOOL,
- fecha_ingreso_cupon DATE NOT NULL
+ fecha_ingreso_cupon DATE DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS cupones_utilizados(
@@ -115,9 +117,9 @@ CREATE TABLE IF NOT EXISTS cupones_utilizados(
 CREATE TABLE IF NOT EXISTS detalles_pedidos (
   id_detalle_pedido INT AUTO_INCREMENT PRIMARY KEY,
   cantidad_detalle_pedido INT,
-  CONSTRAINT precio_producto_check CHECK(cantidad_detalle_pedido >= 0),
-  precio__detalle_pedido DECIMAL(5,2) NOT NULL,
-  CONSTRAINT precio__detalle_pedido_check CHECK(precio__detalle_pedido > 0),
+  CONSTRAINT cantidad_detalle_pedido_check CHECK(cantidad_detalle_pedido > 0),
+  precio_detalle_pedido DECIMAL(5,2) NOT NULL,
+  CONSTRAINT precio_detalle_pedido_check CHECK(precio_detalle_pedido > 0),
   id_producto INT,
   id_pedido INT,
   CONSTRAINT fk_detalles_pedidos_productos FOREIGN KEY (id_producto) REFERENCES productos (id_producto),
@@ -129,10 +131,10 @@ CREATE TABLE IF NOT EXISTS valoraciones (
   calificacion_valoracion INT,
   CONSTRAINT calificacion_valoracion_check CHECK(calificacion_valoracion >= 0),
   comentario_valoracion VARCHAR(250),
-  fecha_valoracion DATE NOT NULL,
+  fecha_valoracion DATE DEFAULT NOW(),
   estado_valoracion BOOL,
-  id_cliente INT,
-  CONSTRAINT fk_valoraciones_clientes FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
+  id_detalle_pedido INT,
+  CONSTRAINT fk_valoraciones_detalles_pedidos FOREIGN KEY (id_detalle_pedido) REFERENCES detalles_pedidos(id_detalle_pedido)
 );
 
 
