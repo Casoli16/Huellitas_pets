c7f6bf2f2db3f31369940681390b2d8c41d8bd3f
Susan Castillo <susancasoli@gmail.com>
2024-03-04 20:31:12 -0600

    Agruege el fixed-top en navbar

--- system_huellitas/controllers/utils/admin.js
+++ system_huellitas/controllers/utils/admin.js
@@ -1,5 +1,5 @@
 const navbar = `
-<nav class="navbar bg-skin-color fixed-top">
+<nav class="navbar bg-skin-color fixed-top sticky-sm-top">
         <div class="container-fluid ">
             <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
                 aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
