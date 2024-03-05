c88baf7b62617f0dbbed2b441e04568e66843728
Susan Castillo <susancasoli@gmail.com>
2024-03-05 09:00:26 -0600

    Cambio de margin top en el dashboard

--- system_huellitas/controllers/utils/components.js
+++ system_huellitas/controllers/utils/components.js
@@ -3,21 +3,21 @@
 const chart = document.getElementById('myChart');
 
 new Chart(chart, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1,
-      backgroundColor: 'rgb(249, 87, 56)'
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1,
+            backgroundColor: 'rgb(249, 87, 56)'
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
     }
-  }
 });
