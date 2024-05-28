5ea08249fe577b22c4d655e0c1c51039d970b888
Cubiasss-Eduardo <20220229@ricaldone.edu.sv>
2024-05-27 21:06:06 -0600

    Haciendo pruebas en categorias URL
    una url no acepta espacios

--- system_huellitas/controllers/public/categorias.js
+++ system_huellitas/controllers/public/categorias.js
@@ -28,7 +28,7 @@ const fillConteiner = async (animal) => {
         DATA.dataset.forEach(row => {
             CONTENEDOR.innerHTML += `
             <div class="col">
-                <div class="card border-0 h-100" data-id="${row.id_categoria}">
+                <div class="card border-0 h-100" name="${row.nombre_categoria}">
                     <img src="${SERVER_URL}images/categorias/${row.imagen_categoria}" class="card-img-top" style="max-height: 500px; min-height: 300px; object-fit: cover;">
                     <div class="card-body">
                         <h5 class="card-title">${row.nombre_categoria}</h5>
@@ -41,10 +41,13 @@ const fillConteiner = async (animal) => {
         // Añadir el evento de clic a todas las tarjetas después de insertarlas en el DOM
         document.querySelectorAll('.card').forEach(card => {
             card.addEventListener('click', () => {
-                const categoria = card.getAttribute('data-id');
+                const categoria = card.getAttribute('name');
+                const resultado = decodeAndRemoveSpaces(`../../views/public/productos.html?categoria=${categoria}`);
+                console.log(resultado);
                 navigateToPage(categoria);
             });
         });
+
     } else {
         sweetAlert(3, DATA.error, true);
     }
@@ -54,3 +57,17 @@ const fillConteiner = async (animal) => {
 const navigateToPage = (categoria) => {
     window.location.href = `../../views/public/productos.html?categoria=${categoria}`;
 }
+
+// Método para devolver una cadena sin espacios al darle una URL codificada
+const decodeAndRemoveSpaces = (encodedURL) => {
+    // Decodificar la URL
+    let decodedString = decodeURIComponent(encodedURL);
+    // Eliminar los espacios de la cadena decodificada
+    let stringWithoutSpaces = decodedString.replace(/\s+/g, ' ');
+    return stringWithoutSpaces;
+}
+
+// Ejemplo de uso (no utilizar en el código actual, solo como referencia)
+// let exampleURL = 'https://example.com/search?query=hola%20mundo';
+// let result = decodeAndRemoveSpaces(exampleURL);
+// console.log(result); // Salida: 'https://example.com/search?query=holamundo'
