257d91a5131dc02cec066337e90f80ed113b2e99
Cubiasss-Eduardo <20220229@ricaldone.edu.sv>
2024-06-09 22:38:02 -0600

    arreglé algo de categorias

--- system_huellitas/api/services/public/productos.php
+++ system_huellitas/api/services/public/productos.php
@@ -233,7 +233,7 @@ if (isset($_GET['action'])) {
                 } else {
                     $result['error'] = 'Este producto no ha sido comentado';
                 }
-                break;
+                break;°
                 //Metodo para leer todos los productos
             case 'readAllProducts':
                 if ($result['dataset'] = $productos->readAllProducts()) {
