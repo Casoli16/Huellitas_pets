90db365ba3f1b831fa8f6b32fb53ff11e1627583
Susan Castillo <susancasoli@gmail.com>
2024-08-13 18:10:12 -0600

    Actualice la contra asociada a los envios del correo

--- system_huellitas/api/helpers/emailConnexion.php
+++ system_huellitas/api/helpers/emailConnexion.php
@@ -22,7 +22,7 @@ class Email
             $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
             $mail->Username   = 'susancasoli@gmail.com';                //SMTP username
-            $mail->Password   = 'rbshysegrfmgslog';                     //SMTP password
+            $mail->Password   = 'eflu fapu wicf gvhe';                     //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
             $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
--- system_huellitas/vendor/composer/installed.php
+++ system_huellitas/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'b7da94ece36b78ad8e80435c161aca86c9025715',
+        'reference' => '9d5d01a4ff369d58e17b11fc2b6bcff6ab9af9e8',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'b7da94ece36b78ad8e80435c161aca86c9025715',
+            'reference' => '9d5d01a4ff369d58e17b11fc2b6bcff6ab9af9e8',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
