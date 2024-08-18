e3caf7823c178898d282d758024f19c01841c86b
Cubiasss-Eduardo <20220229@ricaldone.edu.sv>
2024-08-18 09:43:31 -0600

    Yeaaa funciona, recu para admin y cliente

---
+++ system_huellitas/controllers/public/recuperacion_contra_1.js
@@ -0,0 +1,31 @@
+
+// Constantes para completar las rutas de la API.
+const API = 'services/recuperacion/recuperacion.php';
+
+window.onload = async function () {
+    console.log('Hola desde recuperacion_contra_1.js');
+    const BOTON = document.getElementById('boton');
+    const INPUT1 = document.getElementById('email');
+
+    BOTON.addEventListener('click', async (event) => {
+        if (INPUT1.value === '') {
+            sweetAlert(2, 'El campo del correo no puede estar vacío.', false);
+            return false;
+        }
+            event.preventDefault();
+            
+            const fechaActualUTC = new Date();
+            const FORM = new FormData();
+            FORM.append('correo', INPUT1.value);
+            FORM.append('nivel', 2);
+            FORM.append('fecha', fechaActualUTC);
+            const DATA = await fetchData(API, 'envioCorreo', FORM);
+            if (DATA.status) {
+                sweetAlert(1, DATA.message, true);
+            } else {
+                sweetAlert(2, DATA.error, false);
+                console.error(DATA.exception);
+            }
+    });
+};
+
